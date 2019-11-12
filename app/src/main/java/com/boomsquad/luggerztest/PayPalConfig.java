d31bcedb2e94235374cc2f3d14c2540fff1e8867
Jovon <jovonm.white@gmail.com>
2019-11-12 09:40:52 -0500

    Initial commit

---
+++ app/src/main/java/com/boomsquad/luggerztest/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.boomsquad.luggerztest;
+
+class PayPalConfig {
+    public static final  String PAYPAL_CLIENT_ID = "AdPdNsYWKwdaTv0LGnjQHnRuhWwGcskwL_MkyuuFO97oqkSBaH4xBk2rXaSJ65KdgarA8Xv_HB3DZ8Hv";
+}
